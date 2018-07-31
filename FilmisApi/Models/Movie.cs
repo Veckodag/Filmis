d6162f2f96d76b7ea1e6d168bd85fa8ef2e4c3bd
Fredrik.Sandstrom <Fredrik.Sandstrom@netcompetence.se>
2018-07-31 14:52:36 +0200

    Initial Commit

---
+++ FilmisApi/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FilmisApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+}
---
+++ FilmisApi/Models/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmisApi.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
