0a4653c55adc0f916274a54cf69aefbfa0e309e7
Fredrik Sandström <veckodag@gmail.com>
2018-08-05 11:11:08 +0200

    Seeds the localDB

---
+++ FilmisApi/Models/DBInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace FilmisApi.Models
+{
+    public static class DBInitializer
+    {
+        public static void Initializer(IServiceProvider serviceProvider)
+        {
+            using (var context = new MovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MovieContext>>()))
+            {
+                if (context.Movies.Any() && context.Actors.Any())               
+                    return; // Already seeded!
+
+                context.Actors.AddRange(
+                    new Actor { Name = "Michael Jordan" },
+                    new Actor { Name = "Bill Murray" },
+                    new Actor { Name = "Kyrie Irving" },
+                    new Actor { Name = "Nate Robinson" },
+                    new Actor { Name = "Shaquille O'Neal" }
+                    );
+                context.SaveChanges();
+
+                // Inconventional way to select stuff :D
+                context.Movies.AddRange(
+                    new Movie { Name = "Space Jam", Year = 1996, Actors = context.Actors.Take(2).ToList() },
+                    new Movie { Name = "Uncle Drew", Year = 2018, Actors = context.Actors.Skip(2).Take(3).ToList() },
+                    new Movie { Name = "Groundhog Day", Year = 1993, Actors = context.Actors.Skip(1).Take(1).ToList() }
+                    );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- FilmisApi/Program.cs
+++ FilmisApi/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FilmisApi.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace FilmisApi
@@ -14,7 +16,27 @@ namespace FilmisApi
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //Oldie but a goodie
+            //BuildWebHost(args).Run();
+
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    //Seeding the Database
+                    DBInitializer.Initializer(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Aj då, det här blev inte riktigt som tänkt");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
